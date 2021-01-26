86322ee8746449b3798df37a1e948b4c78ae50c2
Michael Iodice <miodice3@gmail.com>
2021-01-26 16:47:25 -0700

    added function to clear appliance container, watched lecture on better structuring of list to iterate over index and implemented, formatting now corrected

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ function listAppliances(e){
     fetchAppliances()
 }
 
+function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
 
 let appliance
 function addAppliance(e){
@@ -39,21 +44,46 @@ function addAppliance(e){
 }
 
 function fetchAppliances(){
+    // applianceContainer.innerHTML = '';
+    removeAllChildNodes(applianceContainer);
     fetch('http://localhost:3000/appliances')
-    .then(resp=> resp.json())
-    .then(json=> renderAppliances(json))
-}
-
-function renderAppliances(appliances){
-    
-    appliances.forEach(appliance => {
-        appliance = new Appliance(appliance.id, appliance.name)
-        console.log(appliance.name)
-
-        let test = document.createElement('LI')
-        test.className = "formatted"
-        test.innerHTML=`${appliance.name}`
-        applianceContainer.appendChild(test)
+    .then(function(obj){
+        return obj.json()
+    })
+    .then(function(appliancesArray){
+        appliancesArray.forEach(function(appliance){
+            let divCard = document.createElement('div')
+            divCard.setAttribute('id', `appliance-${appliance.id}`)
+            divCard.setAttribute('data-id', `${appliance.id}`)
+            let ul = document.createElement('ul')
+            let li = document.createElement('li')
+            let name = document.createElement('p')
+            // let wattage = document.createElement('p')
+            name.innerText = appliance.name
+            // wattage.innerText = appliance.wattage
+            divCard.appendChild(name)
+            // divCard.appendChild(name, wattage) 
+            applianceContainer.appendChild(divCard)
+        })
     })
 }
+
+// function fetchAppliances(){
+//     fetch('http://localhost:3000/appliances')
+//     .then(resp=> resp.json())
+//     .then(json=> renderAppliances(json))
+// }
+
+// function renderAppliances(appliances){
+//     applianceContainer.children = ""
+
+//     appliances.forEach(appliance => {
+//         appliance = new Appliance(appliance.id, appliance.name)
+  
+//         let test = document.createElement('LI')
+//         test.className = "formatted"
+//         test.innerHTML=`${appliance.name}`
+//         applianceContainer.appendChild(test)
+//     })
+// }
 

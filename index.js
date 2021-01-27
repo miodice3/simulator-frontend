17c21e6af401d96ce5a9b1be2e6ecd3ec850d544
Michael Iodice <miodice3@gmail.com>
2021-01-27 12:49:44 -0700

    fixed list rendering and created event listeners to grab inputs from schedule form & create fetch post request to create new schedule

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 let applianceContainer = document.getElementById("appliance-container")
 let applianceCreationContainer = document.getElementById("appliance-creation-container")
-
 let createAppliance = document.getElementById("create-appliance")
+
+let createSchedule = document.getElementById("appliance-container")
+
+applianceContainer.addEventListener("click", addSchedule)
+// let submitSchedule = document.getElementById("create-schedule")
+// submitSchedule.addEventListener("click", addSchedule)
+
+function addSchedule(e){
+    e.preventDefault()
+    if (e.target.id === "create-schedule") {
+        let example = e.target.dataset.id
+        let ex2 = document.querySelector(`#form-${example}`).hidden = ""
+    }
+
+    if (e.target.id === "add-schedule"){
+        let temp = "form-" + e.target.dataset.id
+        // debugger
+        
+
+        fetch("http://localhost:3000/schedules", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                appliance_id: e.target.dataset.id,
+                day: document.querySelector(`#${temp}`).day.value,
+                time_on: document.querySelector(`#${temp}`).timeon.value,
+                time_off: document.querySelector(`#${temp}`).timeoff.value,
+            })
+            })
+            fetchAppliances()
+    }
+}
 
 createAppliance.addEventListener("click", addAppliance)
 
@@ -72,43 +106,31 @@ function fetchAppliances(){
         appliancesArray.forEach(function(appliance){
             applianceContainer.innerHTML += `
             <div id=appliance-${appliance.id}>
-            <p>${appliance.name} - ${appliance.wattage}</p>
+            <p>${appliance.name} - ${appliance.wattage}w</p>
             <ul>
-                ${appliance.schedules.map(function(schedule){return `<li>${schedule.day}</li>`}).join('')}
+                ${appliance.schedules.map(function(schedule){return `<li>${schedule.day} - on: ${schedule.time_on} off:  - ${schedule.time_off}</li>`}).join('')}
             </ul>
+
+
+            <input id=create-schedule data-id=${appliance.id} type="submit" value="create new schedule">
+            <br>
+
+            <form hidden id=form-${appliance.id}>
+                <label for="day">Select Type:</label>
+                <input type="hidden" id="appliance_id" name="appliance_id" value="${appliance.id}">
+                <select name="day" id="day">
+                <option value="Weekday">Weekday</option>
+                <option value="Weekend">Weekend</option>
+                </select><br>
+                <label for="timeon">Time On (24hr format):</label><br>
+                <input type="text" id="timeon" name="timeon"><br>
+                <label for="timeoff">Time Off (24hr format):</label><br>
+                <input type="text" id="timeoff" name="timeoff"><br>
+                <input id=add-schedule data-id=${appliance.id} type="submit" value="Submit">
+            </form>
+
             </div>
             `
         })
     })
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// function fetchAppliances(){
-//     fetch('http://localhost:3000/appliances')
-//     .then(resp=> resp.json())
-//     .then(json=> renderAppliances(json))
-// }
-
-// function renderAppliances(appliances){
-//     applianceContainer.children = ""
-
-//     appliances.forEach(appliance => {
-//         appliance = new Appliance(appliance.id, appliance.name)
-  
-//         let test = document.createElement('LI')
-//         test.className = "formatted"
-//         test.innerHTML=`${appliance.name}`
-//         applianceContainer.appendChild(test)
-//     })
-// }
-
